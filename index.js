4bdceae0c7e6541065c9110d950a9a6f54133a69
KyTran188 <lycafesuayeuthuong64243@gmail.com>
2023-06-24 14:36:12 +0700

    convert form C temp to F temp

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 console.log("Hello World");
-document.querySelector(".myButton").addEventListener("click", function () {
-  let answer;
-  let myCheckBox = document.querySelector(".subscribeCheckBox");
-  let visaBtn = document.querySelector(".visaBtn");
-  let MasterCardBtn = document.querySelector(".MasterCardBtn");
-  let paypalBtn = document.querySelector(".paypalBtn");
-  if (myCheckBox.checked) {
-    console.log("sucess");
+
+document.querySelector(".buttonSumit").addEventListener("click", function () {
+  let temp = Number(document.querySelector(".input").value);
+  let Cunit = document.querySelector(".Cunit");
+  let Funit = document.querySelector(".Funit");
+  let output = document.querySelector(".tempLabel");
+  if (Cunit.checked) {
+    temp = toC(temp);
+    output.innerHTML = `answer is ${temp}°C`;
+  } else if (Funit.checked) {
+    temp = toF(temp);
+    output.innerHTML = `answer is ${temp}°F`;
   } else {
-    console.log("not responding");
+    output.innerHTML = "Select a unit Please";
   }
-  if (visaBtn.checked) {
-    console.log("you are paying with visa");
-    answer = "visa";
-  } else if (MasterCardBtn.checked) {
-    console.log("you are paying with master card");
-    answer = "master card";
-  } else if (paypalBtn.checked) {
-    console.log("you are paying with paypal");
-    answer = "paypal";
-  } else {
-    console.log("please chose in less one choice");
-    answer = "you did not choose anything";
-  }
-  console.log(answer);
 });
+function toC(temp) {
+  return (temp - 32) * (5 / 9);
+}
+function toF(temp) {
+  return (temp * 9) / 5 + 32;
+}
